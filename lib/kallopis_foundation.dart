e2d9d822ad39c768261e040b70516b07101b2b53
M4ng0D0g <chiayu.myu@icloud.com>
2026-09-20 18:44:36 +0800

    feat: restore controlled document tabs

--- lib/kallopis_foundation.dart
+++ lib/kallopis_foundation.dart
@@ -128,6 +128,7 @@ export 'src/features/navigation/widgets/sidebar/klp_sidebar_navigation_button.da
 export 'src/features/navigation/widgets/sidebar/klp_sidebar_navigation_group.dart';
 export 'src/features/navigation/widgets/sidebar/klp_sidebar_section_label.dart';
 export 'src/features/navigation/widgets/tabs/klp_tabs.dart';
+export 'src/features/navigation/widgets/tabs/klp_document_tabs.dart';
 export 'src/features/navigation/widgets/preview_tree/klp_preview_tree.dart';
 export 'src/features/navigation/widgets/preview_tree/klp_publication_progress_overlay.dart';
 export 'src/features/navigation/widgets/preview_tree/models/klp_preview_tree_node.dart';
